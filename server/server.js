4e44f4e3499c44ebe434d01aaff3a27f7fdb175c
vpratnicki <vanessa@pratnicki.net>
2022-12-18 17:39:30 -0500

    Chore: created the client server

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const { ApolloServer } = require('apollo-server-express');
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-
+const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
 const PORT = process.env.PORT || 3001;
@@ -26,6 +26,15 @@ const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
 // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
